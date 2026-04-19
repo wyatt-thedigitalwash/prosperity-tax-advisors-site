5f4708203c578ffdece44af16b51262ac11600ba
WyattBensman <wyatt@thedigitalwash.com>
2026-04-19 18:00:48 -0400

    Fix sitemap TypeScript error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,6 +3,6 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://DOMAIN.com';
   return [
-    { url: baseUrl, changeFrequency: 'weekly', priority: 1.0 },
+    { url: baseUrl, changeFrequency: 'weekly' as const, priority: 1.0 },
   ].map(page => ({ ...page, lastModified: new Date() }));
 }
